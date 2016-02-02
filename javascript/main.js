9c87b45a31379cb19c0f460cd143683a2bd122e3
Peyton Caseria <PeytonCaseria@gmail.com>
2016-02-02 16:26:54 -0700

    mobile nav t1

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,5 @@
-var s = document.getElementById('thing').style;
-s.opacity = 1;
-(function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
+$( document ).ready(function(){ 
 
-/**$(document).ready(function(){
-    alert('Welcome to Gads browser Toolbox, here you will find tools that you can utilize for all types of purposes. What are you waiting for? Click OK to continue!');
- });
+$(".button-collapse").sideNav();
 
-
-
-$(document).ready(function(){ 
-var x = open.onLoad
-  (document.getElementById("open"))
-
-});
-
-**/
-
+})
